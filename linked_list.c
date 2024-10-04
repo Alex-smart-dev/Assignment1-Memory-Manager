e344f8ea8a1e16d189232bd697c2a3a07fdf4201
Alex-smart-dev <olzh23@student.bth.se>
2024-10-04 23:30:09 +0200

    Add files via upload

--- linked_list.c
+++ linked_list.c
@@ -6,6 +6,7 @@
 #include <inttypes.h>
 #include "linked_list.h"
 #include "memory_manager.h"
+#include "common_defs.h" //remove before last submit !!!!!!!!!!
 
 void list_init(Node** head, size_t size)
 {
@@ -56,15 +57,29 @@ void list_insert_before(Node** head, Node* next_node, uint16_t data)
     Node *newNode = mem_alloc(sizeof(Node));
     newNode->data = data;
 
+    if (*head == next_node)
+    {
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }
+
     Node *walker = *head;
 
-    while (walker->next != next_node)
+    while (walker != NULL && walker->next != next_node)
     {
         walker = walker->next;
     }
 
-    newNode->next = walker->next;
-    walker->next = newNode;
+    if (walker != NULL)
+    {
+        newNode->next = walker->next;
+        walker->next = newNode;
+    }
+    else
+    {
+        mem_free(newNode); // Prevent memory leak if node wasn't inserted
+    }
 }
 
 void list_delete(Node** head, uint16_t data)
@@ -153,21 +168,79 @@ void list_display_range(Node** head, Node* start_node, Node* end_node)
         printf("%" PRIu16", ", start_node->data);
         start_node = start_node->next;
     }
-    printf("%" PRIu16, start_node->data);
-    printf("]");
+    if (end_node == NULL)
+    {
+        printf("%" PRIu16, start_node->data);
+        printf("]");
+    }
+    else
+    {
+        printf("%" PRIu16", ""%" PRIu16, start_node->data, end_node->data);
+        printf("]");
+    }
+    
+    
 }
 
 int list_count_nodes(Node** head)
 {
-    return 0;
+    if (*head == NULL)
+    {   
+        return 0;
+    }
+    
+    Node *walker = *head;
+    int count = 0;
+    while (walker != NULL)
+    {
+        count++;
+        walker = walker->next;
+    }
+    return count;
 }
 
 void list_cleanup(Node** head)
 {
-
+    Node *temp = NULL;
+    while (*head != NULL)
+    {
+        temp = *head;
+        *head = (*head)->next;
+        mem_free(temp);
+    }
+    
 }
 
-void tmain()
+void tmain() //remove before last submit !!!!!!!!!!
 {
+    printf_yellow("  Testing list edge cases ---> ");
+    Node *head = NULL;
+    list_init(&head, sizeof(Node) * 3);
 
+    // Insert at head
+    list_insert(&head, 10);
+    my_assert(head->data == 10);
+
+    // Insert after
+    Node *node = list_search(&head, 10);
+    list_insert_after(node, 20);
+    my_assert(node->next->data == 20);
+
+    // Insert before
+    list_insert_before(&head, node, 15);
+
+    my_assert(head->data == 15);
+    my_assert(head->next->data == 10);
+    my_assert(head->next->next->data == 20);
+
+    // Delete
+    list_delete(&head, 15);
+    my_assert(node->next->data == 20);
+
+    // Search
+    Node *found = list_search(&head, 20);
+    my_assert(found->data == 20);
+
+    list_cleanup(&head);
+    printf_green("[PASS].\n");
 }
